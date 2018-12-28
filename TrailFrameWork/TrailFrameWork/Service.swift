78c55b7064f2bf90a44ed652421f94c54e95b357
Vengababu Maparthi <venga@mobifyi.com>
2018-12-28 11:52:52 +0530

    finish uploaded

---
+++ TrailFrameWork/TrailFrameWork/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  TrailFrameWork
+//
+//  Created by Vengababu Maparthi on 27/12/18.
+//  Copyright © 2018 Vengababu Maparthi. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+    
+    public static func doSomething() -> String{
+        return "successfully integrated framework"
+    }
+    
+}
+
---
+++ TrailFrameWork/TrailFrameWork/TrailFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  TrailFrameWork.h
+//  TrailFrameWork
+//
+//  Created by Vengababu Maparthi on 27/12/18.
+//  Copyright © 2018 Vengababu Maparthi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TrailFrameWork.
+FOUNDATION_EXPORT double TrailFrameWorkVersionNumber;
+
+//! Project version string for TrailFrameWork.
+FOUNDATION_EXPORT const unsigned char TrailFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrailFrameWork/PublicHeader.h>
+
+
